3f1ed7e7f3990daa6b7f9ffbc102e41a0a28258a
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-08-21 19:47:20 -0300

    adiciona mock e listagem de heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Homen Aranha' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Homem de Ferro' },
+  { id: 5, name: 'Mulher Maravilha' },
+  { id: 6, name: 'Pantera Negra' },
+  { id: 7, name: 'Mulher Gato' },
+  { id: 8, name: 'Capitã Marvel' },
+  { id: 9, name: 'Hulk' },
+  { id: 10, name: 'Capitão América' },
+];
