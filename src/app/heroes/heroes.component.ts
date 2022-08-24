1ad86af2d50b058327320bd2cf4719bc58bb70b6
Lucas Marcelino <lucas_marcelino2000@hotmail.com>
2022-08-24 03:32:44 -0300

    add hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,6 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine',
-  };
-
   heroes = HEROES;
   selectedHero?: Hero;
 
